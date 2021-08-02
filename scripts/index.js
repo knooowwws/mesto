83db3141baa5bfe5fd54ad43e38b335d1dfb7e1c
knooowwws <khangereev2001@mail.ru>
2021-08-03 02:57:23 +0400

    sdacha

--- scripts/index.js
+++ scripts/index.js
@@ -1,119 +1,111 @@
-// CONST
-// modalProfile
-const editButton = document.querySelector('.profile__btn-edit');
-const closeButtonProfile = document.querySelector('.popup__close_profile');
-const saveProfileButton = document.querySelector('.popup__submit_profile');
-const modalProfile = document.querySelector('.popup_profile');
-const nameInput = document.querySelector('.form__input_name');
-const jobInput = document.querySelector('.form__input_profile');
-const profileName = document.querySelector('.profile__name').textContent;
-const profileWork = document.querySelector('.profile__work-place').textContent;
-// sixCards
-const cardsList = document.querySelector('.cards');
-const cardTemplate = document.querySelector('.card').content;
+const profile = document.querySelector('.profile');
+let profileName = profile.querySelector('.profile__name');
+let profileWork = profile.querySelector('.profile__work-place');
+const profileEditButton = profile.querySelector('.profile__btn-edit');
 const initialCards = [
     {
-        name: 'Bali',
-        link: 'https://images.unsplash.com/photo-1599459657946-43a884745e2a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8JUQwJUIxJUQwJUIwJUQwJUJCJUQwJUI4fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+        name: 'Бали',
+        url: 'https://images.unsplash.com/photo-1599459657946-43a884745e2a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8JUQwJUIxJUQwJUIwJUQwJUJCJUQwJUI4fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
     },
     {
-        name: 'Pattaya',
-        link: 'https://images.unsplash.com/photo-1599851245208-c1e43c053710?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGF0dGF5YXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+        name: 'Паттайя',
+        url: 'https://images.unsplash.com/photo-1599851245208-c1e43c053710?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGF0dGF5YXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
     },
     {
         name: 'Астрахань',
-        link: 'https://images.unsplash.com/photo-1598005532665-fe6b80edba55?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1866&q=80'
+        url: 'https://images.unsplash.com/photo-1598005532665-fe6b80edba55?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1866&q=80'
     },
     {
-        name: 'Dagestan',
-        link: 'https://images.unsplash.com/photo-1617911478288-ac9559802681?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGFnZXN0YW58ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+        name: 'Дагестан',
+        url: 'https://images.unsplash.com/photo-1617911478288-ac9559802681?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGFnZXN0YW58ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
     },
     {
         name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+        url: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
     },
     {
         name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+        url: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
-const trashCard = document.querySelector('.cards__trash')
-// modalMesto
-const modalMesto = document.querySelector('.popup_mesto');
-const addButton = document.querySelector('.profile__btn-add');
-const closeButtonMesto = document.querySelector('.popup__close_mesto');
-const saveMestoButton = document.querySelector('.popup__submit_mesto');
+
+const cardTemplate = document.querySelector('.card').content;
+
+const modalProfile = document.querySelector('.popup_profile')
+const modalAdd = document.querySelector('.popup_mesto')
+const modalPhoto = document.querySelector('.popup_photo')
+
 
 
 // FUNCTIONS
-closeButtonProfile.addEventListener('click', openCloseModalProfile);
-editButton.addEventListener('click', openCloseModalProfile);
-saveProfileButton.addEventListener('click', changeProfile);
-
-// trashCard.addEventListener('click', deleteCard);
-
-addButton.addEventListener('click', openCloseModalMesto);
-closeButtonMesto.addEventListener('click', openCloseModalMesto);
-saveMestoButton.addEventListener('click', addCard)
-saveMestoButton.addEventListener('click', openCloseModalMesto)
-
-function openCloseModalProfile() {
-    nameInput.value =  profileName;
-    jobInput.value =  profileWork;
-    modalProfile.classList.toggle('popup_opened')
+let openPopup = (popup) => {
+    popup.classList.add('popup_opened')
 }
-function changeProfile(evt) {
+
+let closePopup = (popup) => {
+    popup.classList.remove('popup_opened')
+}
+
+let closeAllPopUps = () => {
+    document.querySelectorAll('.popup__close').forEach(elem => {
+        elem.addEventListener('click', (evt) => closePopup(evt.target.closest('.popup')))
+    })
+}
+closeAllPopUps();
+
+let openModalPhoto = (cardEl) => {
+    modalPhoto.querySelector('.popup__image').src = cardEl.url
+    modalPhoto.querySelector('.popup__name').textContent = cardEl.name
+    openPopup(modalPhoto)
+}
+
+let makeCard = (cardEl) => {
+    const card = cardTemplate.querySelector('.cards__item').cloneNode(true);
+    card.querySelector('.cards__name').textContent = cardEl.name;
+    card.querySelector('.cards__img').src = cardEl.url;
+    card.querySelector('.cards__btn').addEventListener('click', (evt) => {evt.target.classList.toggle('cards__btn_like')})
+    card.querySelector('.cards__trash').addEventListener('click', (evt) => {evt.target.closest('.cards__item').remove()})
+    card.querySelector('.cards__img').addEventListener('click', () => openModalPhoto(cardEl))
+    return card;
+}
+
+let addCards = (cardEl) => {
+    document.querySelector('.cards').prepend(makeCard(cardEl));
+}
+
+let initialCardGenerate = () => {
+    initialCards.forEach(el => {
+        addCards(el)
+    })
+}
+initialCardGenerate();
+
+
+// CALLBACKS
+// profile
+profileEditButton.addEventListener('click', () => {
+    openPopup(modalProfile);
+    modalProfile.querySelector('.form__input_name').value = profileName.textContent;
+    modalProfile.querySelector('.form__input_profile').value = profileWork.textContent;
+})
+
+modalProfile.querySelector('.popup__submit_profile').addEventListener('click', (evt) => {
     evt.preventDefault();
-    document.querySelector('.profile__name').textContent = nameInput.value;
-    document.querySelector('.profile__work-place').textContent = jobInput.value;
-}
+    profileName.textContent = modalProfile.querySelector('.form__input_name').value;
+    profileWork.textContent = modalProfile.querySelector('.form__input_profile').value;
+    closePopup(modalProfile)
+})
 
-
-function openCloseModalMesto() {
-    modalMesto.classList.toggle('popup_opened');
-}
-
-function openModalPhoto (i) {
-    i.classList.toggle('popup_opened');
-
-}
-
-function addCard (evt) {
+// add
+profile.querySelector('.profile__btn-add').addEventListener('click', () => openPopup(modalAdd));
+modalAdd.querySelector('.popup__submit_mesto').addEventListener('click', (evt) => {
     evt.preventDefault();
-    let placeName = modalMesto.querySelector('.form__input_location').value;
-    let placeUrl = modalMesto.querySelector('.form__input_url').value;
-    const card = cardTemplate.cloneNode(true);
-
-    card.querySelector('.cards__img').src = placeUrl;
-    card.querySelector('.cards__name').textContent = placeName;
-    card.querySelector('.cards__btn').addEventListener('click', (evt) => {
-        evt.target.classList.toggle('cards__btn_like');
-        console.log(evt.target)
-    })
-    card.querySelector('.cards__trash').addEventListener('click', (evt) => {
-        evt.target.closest('.cards__item').remove();
-    })
-
-    cardsList.prepend(card);
-}
-
-
-
-function sixCards () {
-    initialCards.forEach( item => {
-        const card = cardTemplate.cloneNode(true);
-        card.querySelector('.cards__img').src = item.link;
-        card.querySelector('.cards__name').textContent = item.name;
-        card.querySelector('.cards__btn').addEventListener('click', (evt) => {
-            evt.target.classList.toggle('cards__btn_like');
-            console.log(evt.target)
-        })
-
-        card.querySelector('.cards__trash').addEventListener('click', (evt) => {
-            evt.target.closest('.cards__item').remove();
-        })
-
-        cardsList.append(card);
-    })
-}
-sixCards();
+    const cardEl = {
+        name: modalAdd.querySelector('.form__input_location').value,
+        url: modalAdd.querySelector('.form__input_url').value
+    }
+    addCards(cardEl);
+    modalAdd.querySelector('.form__input_location').value = ''
+    modalAdd.querySelector('.form__input_url').value = ''
+    closePopup(modalAdd);
+})
